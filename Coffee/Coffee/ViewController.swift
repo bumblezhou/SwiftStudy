ae14549c4cba16f2e55734e6388d14dd5f35c4cf
bumble.zhou <bumble.zhou@gmail.com>
2016-06-15 15:56:45 +0800

    add new project, change folderstructure

---
+++ Coffee/Coffee/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Coffee
+//
+//  Created by Jack Zhou on 6/15/16.
+//  Copyright © 2016 Jack Zhou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
